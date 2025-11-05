dc9487308a03fd60c67fb513e7b51b9fbae7dbc9
hayeon090826-ai <hayeon090826@gmail.com>
2025-11-05 09:25:59 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import streamlit as st
 st.title('집에 보내줘 제발')
-a=st.text_input('너 이름 뭐임?')
-st.selectbox('어떤 음식 좋아해?',['알리오올리오','까르보나라','토마토스파게티'])
+a=st.text_input('너 이름 뭐야?')
+b=st.selectbox('어떤 음식 좋아해?',['알리오올리오','까르보나라','토마토스파게티'])
 if st.button('인사말 생성'):
-  st.write(a+'님, ㅎㅇ. 반갑')
+  st.info(a+', .. 반가워')
+  st.warning(b+'를 좋아하는구나. 나도 뭐..')
+  st.error('..반가워')
+  st.balloons()
